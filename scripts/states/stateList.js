7853d317fb9258d39ae3fa27dc2e0be241a9deee
jandershock <jordan.andershock@gmail.com>
2022-03-21 15:51:03 -0500

    Added default state option

--- scripts/states/stateList.js
+++ scripts/states/stateList.js
@@ -1,7 +1,8 @@
 
 export const stateList = (allStates) => {
     const stateEl = document.querySelector(".stateDropdownDiv")
-	let stateHTML = `<select name="stateDD" id="stateDropdown">`;
+	let stateHTML = `<select name="stateDD" id="stateDropdown">
+		<option>-- Please select state --</option>`;
 		for (const stateObj of allStates) {
 			stateHTML += `<option value="${stateObj.abbreviation}">${stateObj.name}</option>`
 		}
